9d042eec706d4830ba054c3b5cc41fdf43facf19
DatDM <datdm.work@gmail.com>
2022-07-08 21:23:56 +0700

    Add component

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,12 +1,18 @@
+import BuildSource from "pages/BuildSource";
+import Routing from "pages/Routing";
 import routes from "./routes";
 
 export const contents = [
   {
     label: "Build source",
     link: routes.BuildSource,
+    component: BuildSource,
+    isShow: true,
   },
   {
     label: "Routing",
     link: routes.Routing,
+    component: Routing,
+    isShow: true,
   },
 ];
