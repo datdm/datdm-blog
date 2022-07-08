2c6fd5aa9826fccbf4d174203cb7ec6b1cb20e70
DatDM <datdm.work@gmail.com>
2022-07-08 21:34:42 +0700

    Add basename

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter
+    basename={process.env.NODE_ENV === "development" ? "/" : "/technical-react"}
+  >
     <App />
   </BrowserRouter>
 );
