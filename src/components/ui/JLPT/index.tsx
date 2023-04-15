fc04e3eb35fb3644c3aedd92db5229aa6a64091d
DatDM <datdm.work@gmail.com>
2023-04-16 00:28:40 +0900

    Update docx n5

--- src/components/ui/JLPT/index.tsx
+++ src/components/ui/JLPT/index.tsx
@@ -9,18 +9,10 @@ const JLPT = () => {
         .sort(() => Math.random() - 0.5)
         .map((item: any, index: number) => (
           <Box key={index} sx={{ display: "flex", marginBottom: 2 }}>
-            <Box
-              sx={{
-                fontSize: 24,
-                display: "flex",
-                flexDirection: "column",
-                lineHeight: "24px",
-                justifyContent: "flex-end",
-              }}
-            >
-              {index + 1}.
-            </Box>
-            <ReactFuri word={item.word} reading={item.reading} />
+            <ReactFuri
+              word={`${index + 1}. ${item.word}`}
+              reading={`${index + 1}. ${item.reading}`}
+            />
           </Box>
         ))}
     </Box>
