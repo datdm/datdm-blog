13c045ff8501c036d420c7c35812d0bd203c3a0b
DatDM <datdm.work@gmail.com>
2023-06-20 00:15:47 +0900

    Update docx n4

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -43,6 +43,7 @@ import no33 from "assets/data/n4/no33.json"; // done
 import no34 from "assets/data/n4/no34.json"; // done
 import no35 from "assets/data/n4/no35.json"; // done
 import no36 from "assets/data/n4/no36.json"; // done
+import no37 from "assets/data/n4/no37.json"; // done
 
 const HomePage = () => {
   const { srcSelected } = useSelector((state: Store) => state.app);
@@ -99,6 +100,7 @@ const HomePage = () => {
               ...no34.reading,
               ...no35.reading,
               ...no36.reading,
+              ...no37.reading,
             ]}
           />
         );
