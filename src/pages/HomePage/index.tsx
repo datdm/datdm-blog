515ab64b0249d1891ccd403852e01b3925b3e900
DatDM <datdm.work@gmail.com>
2023-07-26 22:57:53 +0900

    Update docx n4

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -55,6 +55,7 @@ import no45 from "assets/data/n4/no45.json"; // done
 import no46 from "assets/data/n4/no46.json"; // done
 import no47 from "assets/data/n4/no47.json"; // done
 import no48 from "assets/data/n4/no48.json"; // done
+import no49 from "assets/data/n4/no49.json"; // done
 
 const HomePage = () => {
   const { srcSelected } = useSelector((state: Store) => state.app);
@@ -123,6 +124,7 @@ const HomePage = () => {
               ...no46.reading,
               ...no47.reading,
               ...no48.reading,
+              ...no49.reading,
             ]}
           />
         );
