47a16d75e03a0abb83a4585a80444306dfee3edb
DatDM <datdm.work@gmail.com>
2023-08-05 01:54:39 +0900

    Update docx n3

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -59,6 +59,7 @@ import no49 from "assets/data/n4/no49.json"; // done
 import no50 from "assets/data/n4/no50.json"; // done
 import no1_1 from "assets/data/n3/no1-1.json"; // done
 import no1_2 from "assets/data/n3/no1-2.json"; // done
+import no1_3 from "assets/data/n3/no1-3.json"; // done
 
 const HomePage = () => {
   const { srcSelected } = useSelector((state: Store) => state.app);
@@ -133,7 +134,11 @@ const HomePage = () => {
           />
         );
       case sources.JLPT_N3:
-        return <JLPT readingData={[...no1_1.reading, ...no1_2.reading]} />;
+        return (
+          <JLPT
+            readingData={[...no1_1.reading, ...no1_2.reading, ...no1_3.reading]}
+          />
+        );
       default:
         return <ReadMarkdown src={srcSelected} />;
     }
